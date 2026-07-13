17e55a7aa59ea2798cebe82a8934d7b0ae77ff8a
John Arley Burns <johnarleyburns@gmail.com>
2026-07-13 04:34:38 -0500

    Enable universal platform support

---
+++ Tests/PlatformSupportTests.swift
@@ -0,0 +1,21 @@
+import XCTest
+
+@testable import Tonearm
+
+final class PlatformSupportTests: XCTestCase {
+    func testAppBundleDeclaresUniversalDeviceFamily() {
+        let families = Bundle.main.object(forInfoDictionaryKey: "UIDeviceFamily") as? [Int]
+
+        XCTAssertEqual(families, [1, 2])
+    }
+
+    func testAppBundleSupportsPortraitAndLandscape() {
+        let orientations = Bundle.main.object(forInfoDictionaryKey: "UISupportedInterfaceOrientations") as? [String]
+
+        XCTAssertEqual(orientations, [
+            "UIInterfaceOrientationPortrait",
+            "UIInterfaceOrientationLandscapeLeft",
+            "UIInterfaceOrientationLandscapeRight",
+        ])
+    }
+}
